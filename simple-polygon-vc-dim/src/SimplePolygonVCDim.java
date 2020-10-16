37596a9c4b3ca11301ac1b462a69df97d1d9ffe2
Matt Gibson <gibson.utsa@gmail.com>
2020-10-16 15:11:16 -0500

    Preparing to dominate.

--- simple-polygon-vc-dim/src/SimplePolygonVCDim.java
+++ simple-polygon-vc-dim/src/SimplePolygonVCDim.java
@@ -4,5 +4,6 @@ public class SimplePolygonVCDim {
 		System.out.println("Hello world!\n");
 		System.out.println("Hi");
 		System.out.println("fast lane");
+		System.out.println("We are going to dominate this problem.");
 	}
 }
