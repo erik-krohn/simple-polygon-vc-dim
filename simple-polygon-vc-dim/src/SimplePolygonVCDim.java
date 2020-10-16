b90f480a54c6f08a43b7d89b548413ddaf015c06
erik-krohn <10359141+erik-krohn@users.noreply.github.com>
2020-10-16 15:23:49 -0500

    added comment
    this better work

--- simple-polygon-vc-dim/src/SimplePolygonVCDim.java
+++ simple-polygon-vc-dim/src/SimplePolygonVCDim.java
@@ -3,7 +3,7 @@ public class SimplePolygonVCDim {
 	public static void main(String[] args) {
 		System.out.println("Hello world!\n");
 		System.out.println("Hi");
-		System.out.println("fast lane");
+		System.out.println("fast lane");//trying to push this back
 		System.out.println("We are going to dominate this problem.");
 	}
 }
